3ad164c40639160d5650b04ee7b23dd39d4af3f1
codecamjam <camerontaghlabi@gmail.com>
2022-05-22 14:06:30 -0400

    l14 click button to change color

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red');
+
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
   return (
     <div>
-      <button style={{ background: 'red' }}>Change to blue</button>
+      <button
+        onClick={() => setButtonColor(newButtonColor)}
+        style={{ background: buttonColor }}
+      >
+        Change to {newButtonColor}
+      </button>
     </div>
   );
 }
