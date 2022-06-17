ed8752af902908778d2e29bbbe7e971cb0980faf
codecamjam <camerontaghlabi@gmail.com>
2022-06-17 10:23:27 -0400

    test commit

--- sundaes-on-demand/src/pages/summary/test/OrderSummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/OrderSummaryForm.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import SummaryForm from "./SummaryForm";
+import SummaryForm from "../SummaryForm";
 
 test("Initial conditions", () => {
   render(<SummaryForm />);
