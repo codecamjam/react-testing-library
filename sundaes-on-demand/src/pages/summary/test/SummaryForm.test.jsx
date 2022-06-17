16bd8265549591598a1d1e9157cc01caf60f0ad3
codecamjam <camerontaghlabi@gmail.com>
2022-06-17 11:52:51 -0400

    l48: not wrapped in act(...) Error, async disappearance

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
 
@@ -27,7 +31,7 @@ test("Checkbox enables button on first click and disables on second click", () =
   expect(confirmButton).toBeDisabled();
 });
 
-test("popover responds to hover", () => {
+test("popover responds to hover", async () => {
   render(<SummaryForm />);
 
   //popover starts out hidden
@@ -45,9 +49,7 @@ test("popover responds to hover", () => {
 
   //popover dissappears when we mouse out
   userEvent.unhover(termsAndConditions);
-  const nullPopoverAgain = screen.queryByText(
-    /no ice cream will actually be delivered/i
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
   );
-
-  expect(nullPopoverAgain).not.toBeInTheDocument();
 });
