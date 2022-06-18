56fc75f154b13d838027718b149894ce940c66f0
codecamjam <camerontaghlabi@gmail.com>
2022-06-18 19:25:07 -0400

    l69: use context to display scoops subtotal

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -4,10 +4,13 @@ import ScoopOption from './ScoopOption';
 import Row from 'react-bootstrap/Row';
 import ToppingOption from './ToppingOption';
 import AlertBanner from '../common/AlertBanner';
+import { pricePerItem } from '../../constants';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
 function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
 
   //optionType is 'scoops' or 'toppings'
   useEffect(() => {
@@ -23,16 +26,29 @@ function Options({ optionType }) {
 
   //TODO: replace null with ToppingOption when available
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, optionType)
+      }
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>
+        {title} total: {orderDetails.totals[optionType]}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
 
 export default Options;
--- sundaes-on-demand/src/pages/entry/ScoopOption.jsx
+++ sundaes-on-demand/src/pages/entry/ScoopOption.jsx
@@ -1,12 +1,35 @@
-import Col from "react-bootstrap/Col";
-function ScoopOption({ name, imagePath }) {
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+function ScoopOption({ name, imagePath, updateItemCount }) {
+  const handleChange = (e) => {
+    updateItemCount(name, e.target.value);
+  };
+
   return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
-        style={{ width: "75%" }}
+        style={{ width: '75%' }}
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} scoop`}
       />
+      <Form.Group
+        controlId={`${name}-count`}
+        as={Row}
+        style={{ marginTop: '10px' }}
+      >
+        <Form.Label column xs="5" style={{ textAlign: 'right' }}>
+          {name}
+        </Form.Label>
+        <Col xs="5" style={{ textAlign: 'left' }}>
+          <Form.Control
+            type="number"
+            defaultValue={0}
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
     </Col>
   );
 }
